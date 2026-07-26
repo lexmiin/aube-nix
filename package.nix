7a1a1ff31b95462c092fc57897d660fa88ff324c
lexmiin <91379959+lexmiin@users.noreply.github.com>
2026-07-26 08:38:18 +0000

    chore: update aube to 1.33.1

--- package.nix
+++ package.nix
@@ -9,21 +9,21 @@
   releases = {
     aarch64-darwin = {
       target = "aarch64-apple-darwin";
-      hash = "sha256-1MiD9qQfBk5YfelIZF2RKNKCsSw6QurDaSDq8dYxYv4=";
+      hash = "sha256-eqnDn+72SHljOO9UD6GviGaD0gBYaR37aRaf5HrqPZk=";
     };
     aarch64-linux = {
       target = "aarch64-unknown-linux-musl";
-      hash = "sha256-b/ueTTMFSFmnCIVv4t3VhYiLwD2rvNnWrDMRlj9rLpY=";
+      hash = "sha256-qWB49knlIbwJnEsVpJ7K7igcs+BIC9bENX2I94UEIbE=";
     };
     x86_64-linux = {
       target = "x86_64-unknown-linux-musl";
-      hash = "sha256-vHSRjCrfiJm4dZmxpD474AH0K+0/tEWJ9pVbynBldVk=";
+      hash = "sha256-oc9KOSU7Fh+a4MOdJqywWMmv3TPD8crzuThKYU5f7Co=";
     };
   };
 in
   stdenvNoCC.mkDerivation rec {
     pname = "aube";
-    version = "1.32.0";
+    version = "1.33.1";
 
     src = let
       system = stdenvNoCC.hostPlatform.system;
